61747e71ae60ccba77f404001c921ccb541b36c6
Joker_Chou <2395294806@qq.com>
2025-04-14 11:27:53 +0800

    1、修改多余代码

--- RabbitMQExample/RabbitMQConsumer/Program.cs
+++ RabbitMQExample/RabbitMQConsumer/Program.cs
@@ -18,13 +18,8 @@ var factory = new ConnectionFactory()
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
-const string exchangeName = "broker.fanout";
+const string exchangeName = "broker.fanout"; 
 
-await channel.ExchangeDeclareAsync(exchange: exchangeName, ExchangeType.Fanout, true, false);
-
-//Direct
-//await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
-//fanout模式下，队列名称可以不一致
 var queue = await channel.QueueDeclareAsync(queue: "", durable: true, exclusive: true, autoDelete: false, arguments: null);
 
 await channel.QueueBindAsync(queue.QueueName, exchangeName, "");
