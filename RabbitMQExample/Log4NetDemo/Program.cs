68cd332552e8ab1c995b47134bbdb661265f103a
zhouhao <Joker_Chou@outlook.com>
2025-06-19 11:31:55 +0800

    1、添加log4net 日志

---
+++ RabbitMQExample/Log4NetDemo/Program.cs
@@ -0,0 +1,38 @@
+using log4net;
+
+namespace Log4NetDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            GlobalContext.Properties["ApplicationName"] = "log4netDemo";
+            builder.Logging.ClearProviders();
+            builder.Logging.AddLog4Net("log4net.config");
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
